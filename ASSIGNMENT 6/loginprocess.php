47d061a7d03cc14e772c12d068be893f6d00df0e
Lavita Gloria Lobo <72128470+Lavi16983@users.noreply.github.com>
2021-08-25 20:07:45 +0530

    Add files via upload

---
+++ ASSIGNMENT 6/loginprocess.php
@@ -0,0 +1,7 @@
+<?php 
+
+$name = $_POST["name"];
+$password = $_POST["password"];
+echo('Name is :'.$name. '<br/>');
+echo('Password is :'.$password. '<br/>');
+?>
---
+++ ASSIGNMENT 6/registerprocess.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$Phone = $_POST["Phone"];
+$password = $_POST["password"];
+$address = $_POST["address"];
+echo('Name is :'.$name. '<br/>');
+echo('Email is  :'.$email. '<br/>');
+echo('Phone is :'.$Phone. '<br/>');
+echo('Password is :'.$password. '<br/>');
+echo('Address is :'.$address. '<br/>');
+
+
+?>
+
